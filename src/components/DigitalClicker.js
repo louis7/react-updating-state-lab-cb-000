b4be76d73fdff929745ede7fc5341c2529ae0cbc
louis baptiste <louisvis@ymail.com>
2020-07-19 16:59:47 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -20,7 +20,7 @@ class DigitalClicker extends React.Component {
   render(){
     return (
       <div>
-      <button onClick ={this.hasBeenClick}>click me </button>
+      <button onClick ={this.hasBeenClick}>click me {this.state.clicked}</button>
       <p>I have been clicked {this.state.clicked} times</p>
       </div>
     )
