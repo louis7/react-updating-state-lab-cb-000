52d3744a80bfc5bcc10817ad0115dda0f10048d4
louis baptiste <louisvis@ymail.com>
2020-07-19 16:46:58 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+class DigitalClicker extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      clicked: 0
+    }
+  }
+  hasBeenClick = ()=> {
+    this.setState (){
+      
+    }
+    
+  }
+  render(){
+    return (
+      <div>
+      <button onClick ={}>click me </button>
+      <p>I have been clicked {} times</p>
+      </div>
+    )
+  }
+}
